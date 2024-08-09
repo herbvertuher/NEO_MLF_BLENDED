4b9fe5e0c8729d43c6813635eb136277014153e8
Yehor <herbvertuher28@protonmail.com>
2024-08-09 23:51:13 +0300

    Revert "structure update"
    This reverts commit d2356e1efa39caeb7fa6b730398d6bf31d64f75a.

---
+++ blended_1_11.05.2024/blended_1.py
@@ -0,0 +1,140 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+# %% 1
+
+df1 = pd.read_csv('./datasets/peoples.csv', sep=',')
+
+# %% 2
+
+df2 = pd.read_csv('./datasets/peoples.csv', index_col='ID')
+
+df2.loc[210]
+#df2[df2['ID'] == 210]
+
+# %% 3
+
+df3 = df2.dropna()
+
+# %% 4
+
+df4 = df2.copy()
+cols_to_drop = ['Name', 'Address', 'Phone']
+df4 = df4.drop(cols_to_drop, axis=1)
+
+# %% 5
+
+nan_mask = df4.isnull().any(axis=1)
+
+df5 = df4[nan_mask]
+
+# %% 6
+
+df6 = df4.drop(index=3)
+df6 = df4[~nan_mask]
+df6 = df4.dropna()
+
+# %% 7
+
+test_df = pd.DataFrame({'one': [1, 2, 3], 
+                        'two': ['a', 'b', 'c']
+                        })
+
+# %% -
+
+new_df = test_df
+
+# %% -
+
+new_df.iloc[0, 1] = 8
+new_df.loc[0, 'two'] = 88
+new_df['two'][0] = 888
+
+# %% 8
+
+duplicates_mask = df6.duplicated()
+
+df7 = df6[~duplicates_mask]
+#df7 = df6.drop_duplicates()
+
+
+# %% 9
+sns.heatmap(df7.corr(numeric_only=True), cmap='coolwarm', annot=True)
+
+
+# %% 10
+df7.dtypes
+
+# %% 11
+
+#df7.groupby('Country')['Salary'].mean()
+
+#df7.groupby('Pet')['Salary'].mean()
+
+#df7.groupby('Qualification')['Salary'].mean()
+
+#df7.groupby(by=['Country', 'Qualification'])['Salary'].mean()
+#df7.groupby(by=['Country', 'Qualification'])['Salary'].apply(np.mean)
+
+# %% 12
+
+df7['Country'].value_counts(normalize=True)
+
+# %% 13
+
+df8 = df7.drop(['Country', 'Pet', 'Qualification'], axis=1)
+
+# %% hist
+
+m = df8.melt()
+
+g = sns.FacetGrid(m,
+                  col='variable',
+                  col_wrap=3,
+                  sharex=False,
+                  sharey=False)
+
+g.map(sns.histplot, 'value')
+
+g.set_titles(col_template='{col_name}')
+
+g.tight_layout()
+
+# %% 14
+
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+
+# %% 15
+
+X_train, X_test, y_train, y_test = train_test_split(
+                                            df8.drop('Salary', axis=1),
+                                            df8['Salary'],
+                                            test_size=0.2,
+                                            random_state=42)
+
+# %% 16
+
+scaler = StandardScaler().set_output(transform='pandas').fit(X_train)
+
+X_train_scaled = scaler.transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# %% 17
+
+model = LinearRegression().fit(X_train_scaled, y_train)
+y_pred = model.predict(X_test_scaled)
+
+
+# %% 18
+
+r_sq = model.score(X_train_scaled, y_train)
+mae = mean_absolute_error(y_test, y_pred)
+mape = mean_absolute_percentage_error(y_test, y_pred)
+
+print(f'R2: {r_sq:.2f} | MAE: {mae:.2f} | MAPE: {mape:.2f}')
+
+# %% 19
---
+++ blended_5_15.06.2024/mod_04_topic_08_svm.py
@@ -0,0 +1,146 @@
+import warnings
+import pandas as pd
+import numpy as np
+import category_encoders as ce
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import KBinsDiscretizer, StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# %%
+
+data = pd.read_csv('../datasets/mod_04_topic_08_petfinder_data.csv.gz')
+data.info()
+
+# %%
+
+data.nunique()
+
+# %%
+
+data['Description'].head()
+
+# %%
+
+data['LenOfDescription'] = data['Description'].str.len()
+
+
+# %%
+data['LenOfDescription'].isna().sum()
+
+# %% 
+
+data['LenOfDescription'] = data['LenOfDescription'].fillna(0).astype(int)
+
+# %%
+
+data.drop('Description', axis=1, inplace=True)
+
+# %%
+
+data['AdoptionSpeed'].value_counts().sort_index()
+
+# %%
+
+data['AdoptionSpeed'] = np.where(data['AdoptionSpeed'] == 4, 0, 1)
+data['AdoptionSpeed'].value_counts()
+
+# %%
+
+data['Fee'] = data['Fee'].astype(bool).astype(int).astype(str)
+
+# %%
+
+X_train, X_test, y_train, y_test = (
+    train_test_split(
+        data.drop('AdoptionSpeed', axis=1),
+        data['AdoptionSpeed'],
+        test_size=0.2,
+        random_state=42))
+
+# %%
+
+num_cols = X_train.select_dtypes(exclude='object').columns
+
+kbins = KBinsDiscretizer(encode='ordinal').fit(X_train[num_cols])
+
+X_train[num_cols] = (kbins
+                     .transform(
+                         X_train[num_cols])
+                     .astype(int)
+                     .astype(str))
+
+X_test[num_cols] = (kbins
+                    .transform(
+                        X_test[num_cols])
+                    .astype(int)
+                    .astype(str))
+
+# %%
+
+encoder = ce.TargetEncoder()
+
+X_train = encoder.fit_transform(X_train, y_train)
+X_test = encoder.transform(X_test)
+
+X_train.head()
+
+# %%
+
+scaler = StandardScaler().set_output(transform='pandas')
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# %%
+
+clf = SVC(class_weight='balanced',
+          kernel='poly',
+          probability=True,
+          random_state=42)
+
+clf.fit(X_train, y_train)
+
+# %%
+
+preds = clf.predict(X_test)
+
+confusion_matrix(y_test, preds)
+
+# %%
+
+print(f'Model accuracy is: {accuracy_score(y_test, preds):.1%}')
+
+# %%
+
+pet = pd.DataFrame(
+    data={
+        'Type': 'Cat', # Dolphin Cat
+        'Age': 3,
+        'Breed1': 'Tabby',
+        'Gender': 'Male',
+        'Color1': 'Black',
+        'Color2': 'White',
+        'MaturitySize': 'Small',
+        'FurLength': 'Short',
+        'Vaccinated': 'No',
+        'Sterilized': 'No',
+        'Health': 'Healthy', # Healthy
+        'Fee': True,
+        'PhotoAmt': 2,
+        'LenOfDescription': 1500
+    },
+    index=[0])
+
+# %%
+
+pet[num_cols] = kbins.transform(pet[num_cols]).astype(int).astype(str)
+
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore')
+    pet_enc = encoder.transform(pet)
+    prob = (clf.predict_proba(scaler.transform(pet_enc)).flatten())
+
+print(f'This pet has a {prob[1]:.1%} probability "of getting adopted"')
