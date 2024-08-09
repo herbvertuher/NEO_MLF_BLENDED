a43837b93e49045970819afa97b8f417f5b99441
Yehor <herbvertuher28@protonmail.com>
2024-08-10 00:02:29 +0300

    structure update

---
+++ lesson-4/blended_4_knn.py
@@ -0,0 +1,162 @@
+import warnings
+import pandas as pd
+import numpy as np
+from scipy.stats import zscore
+from imblearn.over_sampling import SMOTE
+import category_encoders as ce
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import PowerTransformer
+from sklearn.metrics import balanced_accuracy_score, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# %%
+
+data = pd.read_csv('../datasets/mod_04_topic_07_bank_data.csv', sep=';')
+data.head()
+
+# %%
+
+data.drop('duration', axis=1, inplace=True)
+
+# %%
+
+data.describe()
+
+# %%
+
+data.skew(numeric_only=True)
+
+# %%
+
+data = data[zscore(data['campaign']).abs().lt(2)]
+
+# %%
+
+mtx = data.drop('y', axis=1).corr(numeric_only=True).abs()
+
+fig, ax = plt.subplots(figsize=(8, 8))
+
+sns.heatmap(mtx,
+            cmap='crest',
+            annot=True,
+            fmt=".2f",
+            linewidth=.5,
+            mask=np.triu(np.ones_like(mtx, dtype=bool)),
+            square=True,
+            cbar=False,
+            ax=ax)
+
+plt.show()
+
+# %%
+
+data.drop(
+    ['emp.var.rate',
+     'cons.price.idx',
+     'cons.conf.idx',
+     'nr.employed'],
+    axis=1,
+    inplace=True)
+
+# %%
+
+data.select_dtypes(include='object').nunique()
+
+# %%
+
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore')
+
+    data['y'] = data['y'].replace({'no': 0, 'yes': 1})
+
+# %%
+
+X_train, X_test, y_train, y_test = (
+    train_test_split(
+        data.drop('y', axis=1),
+        data['y'],
+        test_size=0.2,
+        random_state=42))
+
+# %%
+
+cat_cols = X_train.select_dtypes(include='object').columns
+cat_cols
+
+# %%
+
+encoder = ce.WOEEncoder(cols=cat_cols)
+
+X_train = encoder.fit_transform(X_train, y_train)
+X_test = encoder.transform(X_test)
+
+# %%
+
+power_transform = PowerTransformer().set_output(transform='pandas')
+
+X_train = power_transform.fit_transform(X_train)
+X_test = power_transform.transform(X_test)
+
+# %%
+
+X_train.skew()
+
+# %%
+
+y_train.value_counts(normalize=True)
+
+# %%
+
+sm = SMOTE(random_state=42, k_neighbors=50)
+X_res, y_res = sm.fit_resample(X_train, y_train)
+
+# %%
+
+knn_mod = KNeighborsClassifier(n_neighbors=15, n_jobs=-1).fit(X_res, y_res)
+
+knn_preds = knn_mod.predict(X_test)
+
+knn_score = balanced_accuracy_score(y_test, knn_preds)
+
+print(f'KNN model accuracy: {knn_score:.1%}')
+
+# %%
+
+gnb_mod = GaussianNB().fit(X_res, y_res)
+
+gnb_preds = gnb_mod.predict(X_test)
+
+gnb_score = balanced_accuracy_score(y_test, gnb_preds)
+
+print(f'GNB model accuracy: {gnb_score:.1%}')
+
+# %%
+
+confusion_matrix(y_test, gnb_preds)
+
+# %%
+
+k_range = range(1, 31, 2)
+ba_scores = []
+
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    ba_scores.append(balanced_accuracy_score(y_test, y_pred))
+
+# Побудуйте графік
+plt.figure(figsize=(15, 9))
+plt.plot(k_range, ba_scores, marker='o')
+plt.xticks(k_range)
+plt.xlabel('Number of Neighbors K')
+plt.ylabel('Balanced Accuracy')
+plt.title('Elbow Method for KNeighborsClassifier')
+plt.grid()
+plt.show()
+
+# %%
+
